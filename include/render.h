88d15dab91aa211c8b05c52ab1e4d6d021351bce
Nga Dinh <55023364+nachtdenkerr@users.noreply.github.com>
2025-07-28 16:47:43 +0200

    header render

--- include/render.h
+++ include/render.h
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thudinh <thudinh@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 16:46:15 by thudinh           #+#    #+#             */
+/*   Updated: 2025/07/28 16:46:44 by thudinh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef RENDER_H
 # define RENDER_H
 
-#include "math_utils.h"
+# include "math_utils.h"
 
-int	hit_sphere(t_sphere sphere, t_ray ray);
+double	hit_sphere(t_sphere sphere, t_ray ray);
+double	hit_cylinder(t_cylinder cyl, t_ray ray);
+double	hit_plane(t_plane plane, t_ray ray);
 
 #endif
