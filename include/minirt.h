ea470024544abbdd56d9caab451b3629557e67c4
Jano <jano.mutschler@outlook.com>
2025-07-29 20:34:27 +0200

    updated incs and make to test parsing

--- include/minirt.h
+++ include/minirt.h
@@ -1,27 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   math_utils.h                                       :+:      :+:    :+:   */
+/*   miniRT.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmutschl <jmutschl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/27 20:37:08 by thudinh           #+#    #+#             */
-/*   Updated: 2025/07/29 16:47:44 by jmutschl         ###   ########.fr       */
+/*   Created: 2025/07/27 19:10:33 by thudinh           #+#    #+#             */
+/*   Updated: 2025/07/29 19:09:10 by jmutschl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATH_UTILS_H
-# define MATH_UTILS_H
+#ifndef MINIRT_H
+# define MINIRT_H
 
 # include "structs.h"
-
-double		vec_dot(t_vector a, t_vector b);
-double		vec_length(t_vector v);
-t_vector	vec_add(t_vector a, t_vector b);
-t_vector	vec_sub(t_vector a, t_vector b);
-t_vector	vec_scale(t_vector v, double s);
-t_vector	vec_cross(t_vector a, t_vector b);
-t_vector	vec_normalize(t_vector v);
-t_vector	vec_reflect(t_vector v, t_vector n);
+# include "parser.h"
+# include "utils.h"
 
 #endif
