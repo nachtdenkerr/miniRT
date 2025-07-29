3e6ffb13cbaa261dfb990e1f7ab6c05d151642bb
Jano <jano.mutschler@outlook.com>
2025-07-29 20:44:50 +0200

    added the parse elements to the first parsing version

---
+++ src/parser/parse_elements.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_elements.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmutschl <jmutschl@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 18:09:39 by jmutschl          #+#    #+#             */
+/*   Updated: 2025/07/29 20:23:03 by jmutschl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+t_vector	parse_vector(char *str, char **tokens, t_scene *scene)
+{
+	t_vector	vec;
+	char		**arr;
+	int			flag;
+
+	flag = 0;
+	arr = ft_split(str, ',');
+	if (!arr)
+		free_and_failed_malloc(tokens, scene);
+	if (!arr[0] || !arr[1] || !arr[2] || arr[3])
+		invalid_argc_exit(tokens, arr, scene, 1);
+	vec.x = ft_atof(arr[0], &flag, 1, 0);
+	if (flag)
+		invalid_vector_val_exit(arr[0], tokens, arr, scene);
+	vec.y = ft_atof(arr[1], &flag, 1, 0);
+	if (flag)
+		invalid_vector_val_exit(arr[1], tokens, arr, scene);
+	vec.z = ft_atof(arr[2], &flag, 1, 0);
+	if (flag)
+		invalid_vector_val_exit(arr[2], tokens, arr, scene);
+	free_arr(arr);
+	return (vec);
+}
+
+t_color	parse_color(char *str, char **tokens, t_scene *scene)
+{
+	t_color		c;
+	char		**arr;
+	int			flag;
+
+	flag = 0;
+	arr = ft_split(str, ',');
+	if (!arr)
+		free_and_failed_malloc(tokens, scene);
+	if (!arr[0] || !arr[1] || !arr[2] || arr[3])
+		invalid_argc_exit(tokens, arr, scene, 1);
+	c.r = ft_atoi_color(arr[0], &flag);
+	if (flag)
+		invalid_color_val_exit(arr[0], tokens, arr, scene);
+	c.g = ft_atoi_color(arr[1], &flag);
+	if (flag)
+		invalid_color_val_exit(arr[1], tokens, arr, scene);
+	c.b = ft_atoi_color(arr[2], &flag);
+	if (flag)
+		invalid_color_val_exit(arr[2], tokens, arr, scene);
+	free_arr(arr);
+	return (c);
+}
+
+void	parse_ambient(char **tokens, t_scene *scene)
+{
+	t_ambient	a;
+	int			flag;
+
+	flag = 0;
+	if (!tokens[1] || !tokens[2] || tokens[3])
+		invalid_argc_exit(tokens, NULL, scene, 7);
+	a.brightness = ft_atof(tokens[1], &flag, 1, 0);
+	if (flag || a.brightness < 0.0 || a.brightness > 1.0)
+		invalid_arg_exit(tokens, scene, 9);
+	a.color = parse_color(tokens[2], tokens, scene);
+	scene->ambient = a;
+}
+
+void	parse_camera(char **tokens, t_scene *scene)
+{
+	t_camera	c;
+	int			flag;
+
+	flag = 0;
+	if (!tokens[1] || !tokens[2] || !tokens[3] || tokens[4])
+		invalid_argc_exit(tokens, NULL, scene, 8);
+	c.position = parse_vector(tokens[1], tokens, scene);
+	c.orientation = parse_vector(tokens[2], tokens, scene);
+	if (check_if_normal(c.orientation))
+		invalid_arg_exit(tokens, scene, 10);
+	c.fov = ft_atof(tokens[3], &flag, 1, 0);
+	if (flag ||c.fov < 0.0 || c.fov > 180.0)
+		invalid_arg_exit(tokens, scene, 11);
+	scene->camera = c;
+}
