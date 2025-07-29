26ac67ba0b91d45cc04edbc795baf5b27cd2a908
Jano <jano.mutschler@outlook.com>
2025-07-29 20:41:25 +0200

    added first parsing version of minirt this is the main parsing file and utils

---
+++ src/parser/parse_scene.c
@@ -0,0 +1,143 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_scene.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmutschl <jmutschl@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 11:40:44 by jmutschl          #+#    #+#             */
+/*   Updated: 2025/07/29 20:28:28 by jmutschl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/parser.h"
+
+void	count_elements(char *line, t_scene *scene)
+{
+	char	**tokens;
+
+	tokens = ft_split(line, ' ');
+	if (!tokens)
+	{
+		free(line);
+		malloc_failure_exit();
+	}
+	free(line);
+	if (!ft_strcmp(tokens[0], "A"))
+		scene->ambiant_count++;
+	else if (!ft_strcmp(tokens[0], "C"))
+		scene->camera_count++;
+	else if (!ft_strcmp(tokens[0], "L"))
+		scene->light_count++;
+	else if (!ft_strcmp(tokens[0], "sp"))
+		scene->sphere_count++;
+	else if (!ft_strcmp(tokens[0], "pl"))
+		scene->plane_count++;
+	else if (!ft_strcmp(tokens[0], "cy"))
+		scene->cylinder_count++;
+	else if (!ft_strcmp(tokens[0], "co"))
+		scene->cone_count++;
+	else
+		unknown_identifier_exit(tokens);
+	free_arr(tokens);
+}
+
+void	get_element_counts(t_scene *scene, char *filename)
+{
+	int		fd;
+	char	*line;
+	char	*tmp;
+
+	line = NULL;
+	tmp = NULL;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		file_open_error_exit(filename);
+	tmp = get_next_line(fd);
+	if (!tmp)
+		return ((void)close(fd));
+	line = ft_strtrim(tmp, "\n");
+	free(tmp);
+	while (line != NULL)
+	{
+		if (is_empty_or_comment(line))
+			free(line);
+		else
+			count_elements(line, scene);
+		tmp = get_next_line(fd);
+		if (!tmp)
+			break ;
+		line = ft_strtrim(tmp, "\n");
+		free(tmp);
+	}
+	close(fd);
+}
+
+void	parse_element(char *line, t_scene *scene)
+{
+	char	**tokens;
+
+	tokens = ft_split(line, ' ');
+	if (!tokens)
+	{
+		free(line);
+		malloc_failure_exit();
+	}
+	free(line);
+	if (!ft_strcmp(tokens[0], "A"))
+		parse_ambient(tokens, scene);
+	else if (!ft_strcmp(tokens[0], "C"))
+		parse_camera(tokens, scene);
+	else if (!ft_strcmp(tokens[0], "L"))
+		parse_light(tokens, scene);
+	else if (!ft_strcmp(tokens[0], "sp"))
+		parse_sphere(tokens, scene);
+	else if (!ft_strcmp(tokens[0], "pl"))
+		parse_plane(tokens, scene);
+	else if (!ft_strcmp(tokens[0], "cy"))
+		parse_cylinder(tokens, scene);
+	else if (!ft_strcmp(tokens[0], "co"))
+		parse_cone(tokens, scene);
+	free_arr(tokens);
+}
+
+void	parse_elements(t_scene *scene, char *filename)
+{
+	int		fd;
+	char	*line;
+	char	*tmp;
+
+	line = NULL;
+	tmp = NULL;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		file_open_error_exit(filename);
+	tmp = get_next_line(fd);
+	if (!tmp)
+		return ((void)close(fd));
+	line = ft_strtrim(tmp, "\n");
+	free(tmp);
+	while (line != NULL)
+	{
+		if (is_empty_or_comment(line))
+			free(line);
+		else
+			parse_element(line, scene);
+		tmp = get_next_line(fd);
+		if (!tmp)
+			break ;
+		line = ft_strtrim(tmp, "\n");
+		free(tmp);
+	}
+	close(fd);
+}
+
+void	parse_scene(char *filename, t_scene *scene)
+{
+	init_element_counts(scene);
+	get_element_counts(scene, filename);
+	validate_element_counts(scene);
+	init_element_arrays(scene);
+	parse_elements(scene, filename);
+	reset_index(scene);
+}
---
+++ src/parser/parse_utils.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmutschl <jmutschl@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 11:40:47 by jmutschl          #+#    #+#             */
+/*   Updated: 2025/07/29 19:50:17 by jmutschl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+int	is_empty_or_comment(const char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i] && (line[i] == ' ' || line[i] == '\t'))
+		i++;
+	if (line[i] == '\0')
+		return (1);
+	if (line[i] == '#')
+		return (1);
+	return (0);
+}
+
+int	check_if_normal(t_vector vec)
+{
+	double	len;
+
+	len = sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+	if (fabs(len - 1) > EPSILON)
+		return (1);
+	return (0);
+}
+
+void	validate_element_counts(t_scene *scene)
+{
+	if (scene->ambiant_count == 0)
+		missing_element_exit(1);
+	else if (scene->ambiant_count > 1)
+		to_much_elements(1);
+	if (scene->camera_count == 0)
+		missing_element_exit(2);
+	else if (scene->camera_count > 1)
+		to_much_elements(2);
+	if (scene->light_count == 0)
+		missing_element_exit(3);
+	if (scene->sphere_count == 0 && scene->plane_count
+		&& scene->cylinder_count == 0 && scene->cone_count == 0)
+		missing_element_exit(4);
+}
+
+int	ft_atoi_color(char *str, int *flag)
+{
+	int	result;
+	int	i;
+
+	i = 0;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	if (str[i] == '-')
+	{
+		*flag = 1;
+		return (0);
+	}
+	while (str[i] >= '0' && str[i] <= '9' && result < 256)
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	if (str[i] != '\0' || result > 255)
+		*flag = 1;
+	return (result);
+}
+
+double	ft_atof(char *str, int *flag, int sign, int i)
+{
+	double	result;
+	double	frac;
+
+	result = 0;
+	frac = 0.1;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	else if (str[i] != '+')
+		i--;
+	i++;
+	while (str[i] >= '0' && str[i] <= '9')
+		result = result * 10 + (str[i++] - '0');
+	if (str[i] == '.')
+	{
+		i++;
+		while (str[i] >= '0' && str[i] <= '9')
+		{
+			result += (str[i++] - '0') * frac;
+			frac *= 0.1;
+		}
+	}
+	if (str[i] != '\0')
+		*flag = 1;
+	return (result * sign);
+}
