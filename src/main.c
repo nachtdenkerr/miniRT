a05e4815480ea781e9d539cca47be59a5e303c16
Jano <jano.mutschler@outlook.com>
2025-08-04 15:35:53 +0200

    added some new functions to test the mlx

--- src/main.c
+++ src/main.c
@@ -6,13 +6,60 @@
 /*   By: jmutschl <jmutschl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 19:10:17 by thudinh           #+#    #+#             */
-/*   Updated: 2025/07/29 20:36:49 by jmutschl         ###   ########.fr       */
+/*   Updated: 2025/08/04 15:27:14 by jmutschl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-int	init_minirt(void)
+void	fill_image_with_color(mlx_image_t *img)
+{
+	for (uint32_t y = 0; y < img->height; ++y)
+	{
+		for (uint32_t x = 0; x < img->width; ++x)
+		{
+			uint32_t r = (x * 255) / img->width;
+			uint32_t g = (y * 255) / img->height;
+			uint32_t b = 255;
+			uint32_t color = (r << 24) | (g << 16) | (b << 8) | 0xFF;
+			mlx_put_pixel(img, x, y, color);
+		}
+	}
+}
+
+void	close_mrt(void *param)
+{
+    t_minirt *mrt;
+
+    mrt = (t_minirt *)param;
+    if (mrt)
+    {
+        if (mrt->img)
+        {
+            mlx_delete_image(mrt->mlx, mrt->img);
+        }
+        if (mrt->mlx)
+        {
+            mlx_close_window(mrt->mlx);
+            mlx_terminate(mrt->mlx);
+        }
+        cleanup_scene(mrt->scene);
+        free(mrt);
+    }
+    exit(0);
+}
+
+void	esc_key(mlx_key_data_t keydata, void *param)
+{
+	t_minirt *mrt = (t_minirt *)param;
+
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+	{
+		close_mrt(mrt);
+	}
+}
+
+t_minirt    *init_minirt(t_scene *scene)
 {
 	mlx_t				*mlx;
 	t_minirt			*mrt;
@@ -30,6 +77,9 @@ int	init_minirt(void)
 			perror(mlx_strerror(mlx_errno)), free(mrt), NULL);
 	mrt->mlx = mlx;
 	mrt->img = image;
+    mrt->scene = scene;
+    mlx_close_hook(mlx, &close_mrt, mrt);
+    mlx_key_hook(mlx, &esc_key, mrt);
 	return (mrt);
 }
 
@@ -38,14 +88,17 @@ int	main(int argc, char **argv)
 	t_scene		scene;
 	t_minirt	*mrt;
 
+    (void)argv;
 	if (argc != 2)
 		error_exit("Usage: ./miniRT scene.rt");
 	parse_scene(argv[1], &scene);
-	mrt = init_minirt();
+	mrt = init_minirt(&scene);
 	if (!mrt)
-		return (EXIT_FAILURE);
-	mrt->scene = &scene;
-	// TODO: render scene here
+		return (cleanup_scene(&scene), EXIT_FAILURE);
+	//minirt(mrt);
+    mlx_image_to_window(mrt->mlx, mrt->img, 0, 0);
+	fill_image_with_color(mrt->img);
+	mlx_loop(mrt->mlx);
 	return (0);
 }
 
