4dc93b307927a019c3d7d09928f55293927274a7
Jano <jano.mutschler@outlook.com>
2025-08-11 10:57:56 +0200

    added triangle placeholder_func

--- src/render/intersection_wrapper.c
+++ src/render/intersection_wrapper.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   intersect_wrapper.c                                :+:      :+:    :+:   */
+/*   intersection_wrapper.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thudinh <thudinh@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: jmutschl <jmutschl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 10:40:20 by thudinh           #+#    #+#             */
-/*   Updated: 2025/08/06 12:41:18 by thudinh          ###   ########.fr       */
+/*   Updated: 2025/08/11 09:44:10 by jmutschl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,11 @@ bool	hit_cylinder_wrapper(void *data, t_ray *ray, t_hit_record *rec)
 bool	hit_cone_wrapper(void *data, t_ray *ray, t_hit_record *rec)
 {
 	return (hit_cone(((t_cone *)data), ray, rec));
+}
+
+bool	hit_triangle_wrapper(void *data, t_ray *ray, t_hit_record *rec)
+{
+	return (hit_triangle(((t_triangle *)data), ray, rec));
 }
 
 bool	hit_object(t_ray *ray, t_scene *scene, t_hit_record *rec)
